bba9c53a84ef33f0cd6b219fea36e8fa4559b63b
Christian <crguado@gmail.com>
2020-11-17 19:43:44 -0500

    Feature/layout (#1)
    * mobile layout preview
    
    * desktop layout preview
    
    * layout finished
    
    * fix: fixed button maximum width
    
    * feat: add plates with javascript
    
    * fix: backgrounds fixed
    
    * change of language and information

---
+++ js/server.js
@@ -0,0 +1,34 @@
+const platesContainer = document.querySelector(".container-grid");
+
+fetch("https://84811ca9bdc9.ngrok.io/plates", {
+  type: "GET",
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((responseJson) => {
+    const plates = responseJson.data.results;
+    let template = ``;
+    for (var i = 0; i < plates.length; i++) {
+      const plate = plates[i];
+      template += `<div class="product__cards-container">
+            <article class="product__card-detail">
+              <figure class="card__img-container">
+                <img
+                  class="card-img"
+                  src=${plate.Image || "assets/hamburguer-img.jpg"}
+                  alt="image card"
+                />
+              </figure>
+              <div class="card-info">
+                <p class="card-info-title">${plate.Nombre}</p>
+                <p class="card-info-description">
+                  ${plate.id}
+                </p>
+                <span>S/${plate.Precio.toFixed(2)}</span>
+              </div>
+            </article>
+          </div>`;
+    }
+    platesContainer.innerHTML = template;
+  });
